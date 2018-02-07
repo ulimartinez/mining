2fc72b53974d7817ffd30b251015c8d2b24133b0
Ulises <ulimartinez96@gmail.com>
2018-02-06 18:06:13 -0700

    initial commit

---
+++ miner_starter.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+repos/ccminer/ccminer -a neoscrypt -i 20 -o stratum+tcp://us1.gobyte.network:4233 -u GUizKcLUUyQRRbu21S5RMq7kjuUgQpYmTf -p x --plimit=250W
---
+++ mining.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+cd /root/mining
+git pull
+./miner_starter.sh
