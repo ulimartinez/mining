3479466587cc0fef36bd545a8bf55b674f374164
ulimartinez <ulimartinez96@gmail.com>
2018-02-07 18:21:45 -0700

    Update daily.sh

--- daily.sh
+++ daily.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
-PATH0/usr/local/sbin:/usr/local/bin:/usr/bin:/opt/cuda/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
-ps -ef | grep min | awk '{print $2}'| xargs kill -9
+PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/opt/cuda/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+#ps -ef | grep min | awk '{print $2}'| xargs kill -9
 cd /root/mining
 git pull
 ./miner_starter.sh
